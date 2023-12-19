1836bba8eaa90d21572241dc13771d6e7ba1f9f4
KyleHtiger <153898567+KyleHtiger@users.noreply.github.com>
2023-12-19 09:51:59 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function submitSelection() {
+    const userName = document.getElementById('userName').value;
+    const selectedNumber = document.getElementById('selectedNumber').value;
+
+    // Send data to the server (to be implemented in the backend).
+    // You can use AJAX, Fetch API, or a library like Axios for this.
+}
